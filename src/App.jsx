ba60e3690389b5bd256a7d7c0180da59edea427e
hp <zekariastamiru12@gmail.com>
2025-01-30 10:39:18 +0300

    enhance layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyle";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: red;
+  /* background-color: red; */
   padding: 58px;
 `;
 
@@ -19,11 +15,29 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The wild oasis</H1>
-        <Button onClick={() => alert("check in")}>Check in</Button>
-        <Button onClick={() => alert("check out")}>Check out</Button>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">The wild Oasis</Heading>
+            <div>
+              <Heading as="h2">CheckOut and in</Heading>
+              <Button
+                variation="secondary"
+                size="large"
+                onClick={() => alert("check in")}
+              >
+                Check in
+              </Button>
+              <Button onClick={() => alert("check out")}>Check out</Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <from>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </from>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
