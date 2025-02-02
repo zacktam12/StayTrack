f23fb266c25f1abb135ab60888dca865252796ff
hp <zekariastamiru12@gmail.com>
2025-02-03 01:35:07 +0300

    add table function

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -6,7 +6,10 @@ const StyledSidBar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.5rem 2.4rem 4.8rem;
   border-right: 1px solid var(--color-gey-100);
-  grid-row: 1/-1;
+  grid-row: 1 / -1;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 function SideBar() {
   return (
