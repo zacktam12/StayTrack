2314538181ac08aec570382df6e87d767577778f
hp <zekariastamiru12@gmail.com>
2025-02-03 01:05:54 +0300

    add cabin management features=

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,19 @@
+import Button from "../../ui/Button";
+import CreateCabinForm from "./CreateCabinForm";
+import Modal from "../../ui/Modal";
+
+function AddCabin() {
+  return (
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
+  );
+}
+export default AddCabin;
