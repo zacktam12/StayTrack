5046ab935c524e4eb2d5aa1eeaefa2b51c50c29b
hp <zekariastamiru12@gmail.com>
2025-02-03 02:21:12 +0300

    add SortBy component

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,21 +1,25 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import Uploader from "../data/Uploader";
 
 const StyledSidBar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.5rem 2.4rem 4.8rem;
   border-right: 1px solid var(--color-gey-100);
+
   grid-row: 1 / -1;
   display: flex;
   flex-direction: column;
   gap: 3.2rem;
 `;
+
 function SideBar() {
   return (
     <StyledSidBar>
       <Logo />
       <MainNav />
+      <Uploader />
     </StyledSidBar>
   );
 }
