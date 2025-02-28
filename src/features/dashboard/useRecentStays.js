6d623eb590b48a804a43705b1ebdfb13c6b9536e
hp <zekariastamiru12@gmail.com>
2025-02-28 23:07:02 +0300

    useRecentStays hook to optimize performance with useMemo

--- src/features/dashboard/useRecentStays.js
+++ src/features/dashboard/useRecentStays.js
@@ -1,24 +1,30 @@
 import { useQuery } from "@tanstack/react-query";
 import { subDays } from "date-fns";
 import { useSearchParams } from "react-router-dom";
+import { useMemo } from "react";
 import { getStaysAfterDate } from "../../services/apiBookings";
 
 export function useRecentStays() {
   const [searchParams] = useSearchParams();
 
-  const numDays = !searchParams.get("last")
-    ? 7
-    : Number(searchParams.get("last"));
-  const queryDate = subDays(new Date(), numDays).toISOString();
+  const numDays = useMemo(() => {
+    return !searchParams.get("last") ? 7 : Number(searchParams.get("last"));
+  }, [searchParams]);
+
+  const queryDate = useMemo(() => {
+    return subDays(new Date(), numDays).toISOString();
+  }, [numDays]);
 
   const { isLoading, data: stays } = useQuery({
     queryFn: () => getStaysAfterDate(queryDate),
     queryKey: ["stays", `last-${numDays}`],
   });
 
-  const confirmedStays = stays?.filter(
-    (stay) => stay.status === "checked-in" || stay.status === "checked-out"
-  );
+  const confirmedStays = useMemo(() => {
+    return stays?.filter(
+      (stay) => stay.status === "checked-in" || stay.status === "checked-out"
+    );
+  }, [stays]);
 
   return { isLoading, stays, confirmedStays, numDays };
 }
