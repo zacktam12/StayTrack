aa0ad42c7a51b753b885511734bfab7139fada64
hp <zekariastamiru12@gmail.com>
2025-03-01 13:23:27 +0300

    chart components for improved structure and readability

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -1,5 +1,6 @@
-import { useDarkMode } from "context/DarkModeContext";
-import { eachDayOfInterval, format, isSameDay, subDays } from "date-fns";
+import styled from "styled-components";
+import DashboardBox from "./DashboardBox";
+import Heading from "../../ui/Heading";
 import {
   Area,
   AreaChart,
@@ -9,9 +10,8 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
-import styled from "styled-components";
-import Heading from "ui/Heading";
-import DashboardBox from "./DashboardBox";
+import { useDarkMode } from "../../context/DarkModeContext";
+import { eachDayOfInterval, format, isSameDay, subDays } from "date-fns";
 
 const StyledSalesChart = styled(DashboardBox)`
   grid-column: 1 / -1;
@@ -24,7 +24,6 @@ const StyledSalesChart = styled(DashboardBox)`
 `;
 
 function SalesChart({ bookings, numDays }) {
-  // In the chart we need to set colors, but we can't do it based on CSS variables, because we have no access to them here. So let's set them manually
   const { isDarkMode } = useDarkMode();
 
   const allDates = eachDayOfInterval({
@@ -60,13 +59,12 @@ function SalesChart({ bookings, numDays }) {
 
   return (
     <StyledSalesChart>
-      <Heading type="h2">
+      <Heading as="h2">
         Sales from {format(allDates.at(0), "MMM dd yyyy")} &mdash;{" "}
-        {format(allDates.at(-1), "MMM dd yyyy")}
+        {format(allDates.at(-1), "MMM dd yyyy")}{" "}
       </Heading>
 
-      <ResponsiveContainer width="100%" height={300}>
-        {/* <AreaChart data={data} width={700} height={300}> */}
+      <ResponsiveContainer height={300} width="100%">
         <AreaChart data={data}>
           <XAxis
             dataKey="label"
@@ -81,26 +79,22 @@ function SalesChart({ bookings, numDays }) {
           <CartesianGrid strokeDasharray="4" />
           <Tooltip contentStyle={{ backgroundColor: colors.background }} />
           <Area
-            type="monotone"
             dataKey="totalSales"
-            // stroke='#4f46e5'
-            // fill='#c7d2fe'
+            type="monotone"
             stroke={colors.totalSales.stroke}
             fill={colors.totalSales.fill}
             strokeWidth={2}
-            unit="$"
             name="Total sales"
+            unit="$"
           />
           <Area
-            type="monotone"
             dataKey="extrasSales"
-            // stroke='#15803d'
-            // fill='#dcfce7'
+            type="monotone"
             stroke={colors.extrasSales.stroke}
             fill={colors.extrasSales.fill}
             strokeWidth={2}
-            unit="$"
             name="Extras sales"
+            unit="$"
           />
         </AreaChart>
       </ResponsiveContainer>
@@ -109,35 +103,3 @@ function SalesChart({ bookings, numDays }) {
 }
 
 export default SalesChart;
-
-const OLDdata = [
-  { label: "Jan 09", totalSales: 480, extrasSales: 320 - 300 },
-  { label: "Jan 10", totalSales: 580, extrasSales: 400 - 300 },
-  { label: "Jan 11", totalSales: 550, extrasSales: 450 - 300 },
-  { label: "Jan 12", totalSales: 600, extrasSales: 350 - 300 },
-  { label: "Jan 13", totalSales: 700, extrasSales: 550 - 300 },
-  { label: "Jan 14", totalSales: 800, extrasSales: 650 - 500 },
-  { label: "Jan 15", totalSales: 700, extrasSales: 700 - 500 },
-  { label: "Jan 16", totalSales: 650, extrasSales: 500 - 300 },
-  { label: "Jan 17", totalSales: 600, extrasSales: 600 - 300 },
-  { label: "Jan 18", totalSales: 550, extrasSales: 400 - 300 },
-  { label: "Jan 19", totalSales: 700, extrasSales: 600 - 500 },
-  { label: "Jan 20", totalSales: 800, extrasSales: 700 - 500 },
-  { label: "Jan 21", totalSales: 700, extrasSales: 600 - 500 },
-  { label: "Jan 22", totalSales: 810, extrasSales: 550 - 500 },
-  { label: "Jan 23", totalSales: 950, extrasSales: 750 - 500 },
-  { label: "Jan 24", totalSales: 970, extrasSales: 600 - 500 },
-  { label: "Jan 25", totalSales: 900, extrasSales: 700 - 500 },
-  { label: "Jan 26", totalSales: 950, extrasSales: 800 - 500 },
-  { label: "Jan 27", totalSales: 850, extrasSales: 700 - 500 },
-  { label: "Jan 28", totalSales: 900, extrasSales: 600 - 500 },
-  { label: "Jan 29", totalSales: 800, extrasSales: 800 - 500 },
-  { label: "Jan 30", totalSales: 950, extrasSales: 700 - 500 },
-  { label: "Jan 31", totalSales: 1100, extrasSales: 800 - 500 },
-  { label: "Feb 01", totalSales: 1200, extrasSales: 900 - 500 },
-  { label: "Feb 02", totalSales: 1250, extrasSales: 800 - 500 },
-  { label: "Feb 03", totalSales: 1400, extrasSales: 950 - 500 },
-  { label: "Feb 04", totalSales: 1500, extrasSales: 1000 - 500 },
-  { label: "Feb 05", totalSales: 1400, extrasSales: 1100 - 500 },
-  { label: "Feb 06", totalSales: 1450, extrasSales: 900 - 500 },
-];
--- src/features/dashboard/Stats.jsx
+++ src/features/dashboard/Stats.jsx
@@ -1,53 +1,53 @@
 import {
+  HiOutlineBanknotes,
   HiOutlineBriefcase,
   HiOutlineCalendarDays,
-  HiOutlineBanknotes,
   HiOutlineChartBar,
 } from "react-icons/hi2";
-import { formatCurrency } from "utils/helpers";
 import Stat from "./Stat";
+import { formatCurrency } from "../../utils/helpers";
 
 function Stats({ bookings, confirmedStays, numDays, cabinCount }) {
-  // Stat 1)
+  // 1.
   const numBookings = bookings.length;
 
-  // Stat 2)
+  // 2.
   const sales = bookings.reduce((acc, cur) => acc + cur.totalPrice, 0);
 
-  // Stat 3)
+  // 3.
   const checkins = confirmedStays.length;
 
-  // Stat 4)
-  // We will use a trick to calculate occupancy rate. It's not 100% accurate, but we want to keep it simple. We know we can have a total of 'numDays * cabinCount' days to occupy, and we also know how many days were actually booked. From this, we can compute the percentage
+  // 4.
   const occupation =
     confirmedStays.reduce((acc, cur) => acc + cur.numNights, 0) /
     (numDays * cabinCount);
+  // num checked in nights / all available nights (num days * num cabins)
 
   return (
     <>
       <Stat
-        icon={<HiOutlineBriefcase />}
         title="Bookings"
-        value={numBookings}
         color="blue"
+        icon={<HiOutlineBriefcase />}
+        value={numBookings}
       />
       <Stat
-        icon={<HiOutlineBanknotes />}
         title="Sales"
-        value={formatCurrency(sales)}
         color="green"
+        icon={<HiOutlineBanknotes />}
+        value={formatCurrency(sales)}
       />
       <Stat
-        icon={<HiOutlineCalendarDays />}
         title="Check ins"
-        value={checkins}
         color="indigo"
+        icon={<HiOutlineCalendarDays />}
+        value={checkins}
       />
       <Stat
-        icon={<HiOutlineChartBar />}
         title="Occupancy rate"
-        value={Math.round(occupation * 100) + "%"}
         color="yellow"
+        icon={<HiOutlineChartBar />}
+        value={Math.round(occupation * 100) + "%"}
       />
     </>
   );
